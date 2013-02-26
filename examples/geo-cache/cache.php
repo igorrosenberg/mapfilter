d7277e6f9f29c3f4848223100b42fbbc3f0eb6d5
igorrosenberg <igor.rosenberg@gmail.com>
2013-02-27 00:12:43 +0100

    specific php cache 1

--- examples/geo-cache/cache.php
+++ examples/geo-cache/cache.php
@@ -1,20 +1,27 @@
 <?php
 
-  $db = @ mysql_connect('localhost', 'DBUSER', 'DBPASSWORD');
+require 'passwords.php';
+
+  $db = @mysql_connect($host, $user, $password) or die('database connection error: ' + mysql_error());
+    
+  mysql_select_db($dbname, $db) or die('No such schema ' + $dbname +': '+ mysql_error());
   
-  if (!$db) {
-    die("Sorry, database error!");
-  }
+   // write to db
+
+  address = 'UPPER(' . mysql_real_escape_string($_GET['address']) . ')';
+
+  $latitude =  48.00 + date('H:i:s') / 300; 
+  $longitude = -1.70 + date('H:i:s') / 300;
+  $sql = 'INSERT into geocode ( address, latitude, longitude, date_added )' . 
+         " VALUES ( $address, $latitude, $longitude, NOW() );"
   
-  mysql_select_db('DBNAME',$db);
-  
-  $sql = "INSERT into `postcodes` (  `postcode` ,  `latitude` ,  `longitude` ,  `date_added` ) VALUES (UPPER('" . mysql_real_escape_string($_GET['postcode']) . "'),  '" . mysql_real_escape_string($_GET['latitude']) . "',  '" . mysql_real_escape_string($_GET['longitude']) . "', NOW( ))";
-  
-  if ($result = mysql_query($sql))
-  {
-    echo "Success";
-  }else{
-    echo "Problem";
-  }
-  
+  $result = mysql_query($sql) or die ('Problem inserting data in database: ' + mysql_error());
+
+   // read from db
+
+  $sql = 'SELECT latitude,longitude FROM geocode ' +
+	  " WHERE address LIKE '$address' LIMIT 1;"
+  $result = mysql_query($sql) or die ('Problem reading data from database: ' + mysql_error());
+  $row = mysql_fetch_assoc($result);
+  echo $row['latitude'] . ',' . $row['longitude']);
 ?>
