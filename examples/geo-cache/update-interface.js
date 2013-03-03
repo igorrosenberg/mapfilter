d42a98a192b369f4db0323b83e1bedd6368fcff8
igorrosenberg <igor.rosenberg@gmail.com>
2013-03-03 01:30:44 +0100

    called google via google API

--- examples/geo-cache/update-interface.js
+++ examples/geo-cache/update-interface.js
@@ -4,35 +4,50 @@ window.addEventListener("load", init, false);
 function init() {
 	var button = document.getElementById ("new_task_button");
 	button.addEventListener("click", startAction, false);
+	
+	var script = document.createElement('script');
+	script.type = 'text/javascript';
+	script.src = 'https://maps.googleapis.com/maps/api/js?v=3.10&sensor=false&callback=googleMapsLoaded';
+	document.body.appendChild(script);
+	}            
+
+// has the map loading script finished ?
+var googleGeocoder;
+
+function googleMapsLoaded() {	
+	googleGeocoder = new google.maps.Geocoder();
 }
 
 function startAction(event) {
 	console.log("start geocode");
 	var password =  document.getElementById ("password").value;
 	var nb_entries =  document.getElementById ("nb_entries").value;
-	var cache_update_delay =  document.getElementById ("cache_update_delay").value;
 	var geocode_poll_min_delay =  document.getElementById ("geocode_poll_min_delay").value;
-
-	getPendingAdresses(password, nb_entries, cache_update_delay, geocode_poll_min_delay, geocode);
-	
+	getPendingAdresses(password, nb_entries, geocode_poll_min_delay);
 	event.preventDefault();
 }
 
-function	getPendingAdresses(pass, limit, cache_update_delay, geocode_poll_min_delay, geocodeFunction){
-	console.log("    " + nb_entries + " " + cache_update_delay + " " + geocode_poll_min_delay);
-	console.log("Need to call the cache service to read new address data ");
-	console.log("Then call geocode");
-	console.log("Which will call updateCache");
+function	getPendingAdresses(pass, limit, geocode_poll_min_delay){
+	console.log("    " + limit + " " + geocode_poll_min_delay);
 	var fetchUrl = 'update.php';
 	var ajaxURL = fetchUrl + "?pass=" + pass + "&limit="  + limit;
 	var xmlhttp=new XMLHttpRequest();
 	xmlhttp.onreadystatechange = function () {
 		if (xmlhttp.readyState==4) {
 			if (xmlhttp.status==200) {
-				 info ('response received (list of addresses to geocode), length=' + xmlhttp.responseText.length);
-				 parsePendingAdresses(xmlhttp.responseText, pass, cache_update_delay, geocode_poll_min_delay, geocodeFunction);
+				 console.log ('response received (list of addresses to geocode), length=' + xmlhttp.responseText.length);
+				 var addresses = parsePendingAdresses(xmlhttp.responseText);
+				 for (var i=0 ; i < addresses.length; i++){
+				 	var addr = addresses[i];
+				 	 geocode(addr);
+/*				 	
+					setTimeout(function() {
+					 	 geocode(addr);
+					}, geocode_poll_min_delay);
+*/					
+				 }
 			} else {
-				 info ('Pending addresses could not be fetched... status=' + xmlhttp.status);
+				 console.log ('Pending addresses could not be fetched... status=' + xmlhttp.status);
 			}
 		 }
 	  };
@@ -40,17 +55,43 @@ function	getPendingAdresses(pass, limit, cache_update_delay, geocode_poll_min_de
 	xmlhttp.send();
 }
 
-function parsePendingAdresses(responseText, pass, cache_update_delay, geocode_poll_min_delay, geocodeFunction){
+function parsePendingAdresses(responseText) {
 	console.log ('Should now decode the json string: ' + responseText);
 	var response = JSON.parse(responseText);
+	var addressArray = []; 
 	if (response.status === 'OK'){
-		console.log ('use this: ' + response);
+		for (var answer in response.keys) {
+			addressArray.push(response.keys[answer].address);
+		}
 	} else { 
 		console.log ('Error when trying to get the addresses: ' + response.message);
-	}
+	}	
+	console.log ('Returning: ' + addressArray );
+	return addressArray; 
 }
 
 function	updateCache() {
 	console.log("Need to call the cache service to write new geolocation data ");
+}
+
+
+function geocode(address) {
+	console.log ('geocoding event: ' + address);
+	googleGeocoder.geocode({'address': address}, function(results, status){
+		console.log ('Geocode response received' + status);
+		if (status != 'OK') {
+			console.log ("encodage GPS d'adresse impossible: " + status + ' pour ' + address);
+			return; 
+			}
+		pushLatLong(address, results[0].geometry.location);
+	});
+}
+
+
+function pushLatLong(address, point) {
+ 	var lat = point.lat(); 
+	var lng = point.lng();
+	console.log ('received: address=' + address + ' google=' + lat + '/' + lng);
+	console.log("call updateCache");
 }
 
