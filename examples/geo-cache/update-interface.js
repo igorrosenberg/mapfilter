0e66bbaae24177738b73cd2c96c25d53382209f6
default_git_name <default_git_email>
2013-03-06 23:46:12 +0000

    progress bar basics

--- examples/geo-cache/update-interface.js
+++ examples/geo-cache/update-interface.js
@@ -5,6 +5,7 @@ function init() {
 	var button = document.getElementById ("new_task_button");
 	button.addEventListener("click", startAction, false);
 	
+	// async load google geocode functions
 	setTimeout(function() { 
   		var script = document.createElement('script');
 		script.type = 'text/javascript';
@@ -20,16 +21,63 @@ function googleMapsLoaded() {
 	googleGeocoder = new google.maps.Geocoder();
 }
 
+var latest_batch_id=0;
+
+function createProgressDiv() {
+  	var div = document.createElement('li');
+	
+	// or use div.setAttribute ('id', xxx);
+	div.id = 'p' + (latest_batch_id++);
+        
+        div.appendChild(document.createTextNode('reading cache')); 
+  	var detail = document.createElement('div');
+  	detail.id = div.id + 'd';
+  	div.appendChild(detail);
+	div.onmouseover = "show("+detail.id+")";
+	div.onmouseout = "hide("+detail.id+")";
+	
+	// use css for that
+	detail.style="display:none;"
+        
+        document.getElementById("queue_ul").appendChild(div);
+
+	return div.id;
+	}
+
+// generic, move to other js file
+function show(id) {
+   var element = document.getElementById(id);
+   element.style.display = 'block';
+   }
+      
+function hide() {
+   var element = document.getElementById(id);
+   element.style.display = 'none';
+   }
+   
+function addProgress(type,id){
+  	var block = document.createElement('span');
+// or use div.setAttribute ('id', xxx);
+  	span.class = type;
+        document.getElementById(id).appendChild(block);
+}
+
+function removeProgress(parent_id){
+        var parent = document.getElementById(parent_id);
+        parent.removeChild(parent.firstChild);
+}
+
 function startAction(event) {
 	console.log("start geocode " );
 	var password =  document.getElementById ("password").value;
 	var nb_entries =  document.getElementById ("nb_entries").value;
 	var geocode_poll_min_delay =  document.getElementById ("geocode_poll_min_delay").value;
-	getPendingAdresses(password, nb_entries, geocode_poll_min_delay);
+	var progressDivId = createProgressDiv();
+	getPendingAdresses(password, nb_entries, geocode_poll_min_delay, progressDivId);
 	event.preventDefault();
 }
 
-function getPendingAdresses(pass, limit, geocode_poll_min_delay){
+function getPendingAdresses(pass, limit, geocode_poll_min_delay, batch_id){
 	console.log("    " + limit + " " + geocode_poll_min_delay);
 	var fetchUrl = 'update.php';
 	var ajaxURL = fetchUrl + "?pass=" + pass + "&limit="  + limit;
@@ -39,7 +87,7 @@ function getPendingAdresses(pass, limit, geocode_poll_min_delay){
 			if (xmlhttp.status==200) {
 				 console.log ('response received (list of addresses to geocode), length=' + xmlhttp.responseText.length);
 				 var addresses = parsePendingAdresses(xmlhttp.responseText);
-				 geocode(addresses, geocode_poll_min_delay, pass);
+				 geocode(addresses, geocode_poll_min_delay, pass, batch_id);
 			} else {
 				 console.log ('Pending addresses could not be fetched... status=' + xmlhttp.status);
 			}
@@ -64,12 +112,8 @@ function parsePendingAdresses(responseText) {
 	return addressArray; 
 }
 
-function updateCache() {
-	console.log("Need to call the cache service to write new geolocation data ");
-}
-
-
-function geocode(addresses, geocode_poll_min_delay, pass) {
+// tail recursive => make as a for loop on array
+function geocode(addresses, geocode_poll_min_delay, pass, batch_id) {
 	if ( (!addresses) || addresses.length == 0 )
 		return;
 	var address = addresses.shift();
@@ -77,24 +121,26 @@ function geocode(addresses, geocode_poll_min_delay, pass) {
 	googleGeocoder.geocode({'address': address}, function(results, status){
 		console.log ('Geocode response received' + status);
 		if (status == 'OK') {
-			pushLatLong(address, results[0].geometry.location, pass);
+			pushLatLong(address, results[0].geometry.location, pass, batch_id);
 		} else {
 			console.log ("encodage GPS d'adresse impossible: " + status + ' pour ' + address);
 			if (status == 'OVER_QUERY_LIMIT') {
 				var justOne = [address];
 				var adjusted_delay = 5*geocode_poll_min_delay;
-				setTimeout(function() { geocode(justOne, adjusted_delay, pass); }, adjusted_delay);
+				setTimeout(function() { geocode(justOne, adjusted_delay, pass, batch_id); }, adjusted_delay);
 			}
 		}
 	});
-	setTimeout(function() { geocode(addresses, geocode_poll_min_delay, pass); }, geocode_poll_min_delay); 
+	setTimeout(function() { geocode(addresses, geocode_poll_min_delay, pass, batch_id); }, geocode_poll_min_delay); 
 }
 
 
-function pushLatLong(address, point, pass) {
+function pushLatLong(address, point, pass, log_id) {
  	var lat = point.lat(); 
 	var lng = point.lng();
 	console.log ('received: address=' + address + ' google=' + lat + '/' + lng);
+
+
 
 	var fetchUrl = 'update.php';
 	var ajaxURL = fetchUrl + "?pass=" + pass + "&address=" + address + "&latitude=" + lat + "&longitude=" + lng ;
@@ -102,21 +148,22 @@ function pushLatLong(address, point, pass) {
 	xmlhttp.onreadystatechange = function () {
 		if (xmlhttp.readyState==4) {
 			if (xmlhttp.status==200) {
-				 console.log ('pushed lat n long for ' + address + ' response ' + xmlhttp.responseText);
-				report ('Done: ' + address);
+				console.log ('pushed lat n long for ' + address + ' response ' + xmlhttp.responseText);
+				report ('Cached: ' + address, 'ok');
 			} else {
-				 console.log ('Cannot push lat n long:' + xmlhttp.status);
+				console.log ('Cannot push lat n long:' + xmlhttp.status);
+				report ('Not cached: ' + address, 'fail');
 			}
 		 }
 	  };
 	xmlhttp.open('GET', ajaxURL, true);
 	xmlhttp.send();
-
 }
 
 
-function report(string){
+function report(string, css_classes){
                 var content = document.createElement('li');
-                content.appendChild(document.createTextNode(string));
+                content.addClass(css_classes);
+                content.appendChild(document.createTextNode(string)); 
                 document.getElementById("queue_ul").appendChild(content);
 }
