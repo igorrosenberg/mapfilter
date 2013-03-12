ca0f97243b23e0bce6aac10bb6f4a918facd7250
igorrosenberg <igor.rosenberg@gmail.com>
2013-03-12 21:21:29 +0000

    tentative geocache

--- examples/gcm/js/geocode.js
+++ examples/gcm/js/geocode.js
@@ -1,47 +1,80 @@
 var geoCodeCalls = 0;
 
-var googleGeocoder ; 
+var googleGeocoder; 
+
+var geocodeCacheUrl = 'http://igor.rosenberg.free.fr/gc/m/geo-cache/cache.php?address=';
 
 function geocode(eventArray, callback) {	
+	geoCodeCalls += eventArray.length;
+ 	for (var i = 0; i < eventArray.length; i++) {
+		geocodeOneEvent(eventArray[i].addrOrig, callback);
+		}
+	// call to createMap=callback made in success
+}
+
+
+function geocodeGoogle(address, callback) {
 	// loop on this function until google maps ready
 	if (!mapScriptLoaded){
 		console.log ('map script not yet ready ??');
-		setTimeout(function(){geocode(eventArray, callback);}, 50);
+		setTimeout(function(){geocodeGoogle(address, callback);}, 50);
 		return;
 	}
 	if (! googleGeocoder )	
 		googleGeocoder = new google.maps.Geocoder();
 
-	for (var i = 0; i < eventArray.length; i++) {
-		var event = eventArray[i];
-		geocodeOneEvent(event, callback);
-		}
-	// call to createMap made in geocodeOneEvent
-}
+	// https://developers.google.com/maps/documentation/javascript/v2/services?hl=es#Geocoding_Object
 
+	googleGeocoder.geocode({'address': address}, function(results, status){
+		if (status != 'OK') {
+			console.warn ("encodage GPS d'adresse impossible: " + status + ' pour ' + address);
+			success (callback); 
+		} else {
+			var point = results[0].geometry.location;
+			localCache[address] = {lat:point.lat();lng: point.lng()};
+			success (callback); 
+			}
+	});
+} // end geocodeGoogle
+
+function success( callback ){
+	geoCodeCalls--; 
+	if (geoCodeCalls == 0) { 
+		callback(); 
+		}
+         now see/delete addLatLong(event, point)
+}
 function addLatLong(event, point){
 	event.lat = point.lat(); 
 	event.lng = point.lng();
 	event_markers.add(event);
 }
 
-function geocodeOneEvent(event, callback) {
-	info ('geocoding event: ' + event.addrOrig);
-	geoCodeCalls ++ ;
-	// https://developers.google.com/maps/documentation/javascript/v2/services?hl=es#Geocoding_Object
 
-	googleGeocoder.geocode({'address': event.addrOrig}, function(results, status){
-		geoCodeCalls--;
-		if (status != 'OK') {
-			info ("encodage GPS d'adresse impossible: " + status + ' pour ' + event.addrOrig);
-			return; 
-			}
-		var point = results[0].geometry.location;
-		addLatLong(event, point); 
+function geocodeOneEvent(address, callback) {
+	if (localCache[address] == null)
+		geocodeCache(address, callback);
+	else 
+		success(callback);
+}
 
-		if (geoCodeCalls == 0) {
-		   callback();
-		}
-	});
-} // end geocodeOneEvent
+function geocodeCache(address, callback) {
+	var ajaxURL = geocodeCacheUrl + address;
+	Par xmlhttp = new XMLHttpRequest();
+	xmlhttp.onreadystatechange = function () {
+		if (xmlhttp.readyState==4) {
+			if (xmlhttp.status==200) {
+				 info ('Cache response received, length=' + xmlhttp.responseText.length);
+				 localCache[address] = parseCacheResponse(xmlhttp.responseText);
+				success(callback);
+			} else {
+				console.warn ('Cache response failure... status=' + xmlhttp.status);
+				geocodeGoogle(address, callback);
+				 }
+		 }
+	  };
+
+	xmlhttp.open("GET",ajaxURL,true);
+	xmlhttp.send();
+}
 
