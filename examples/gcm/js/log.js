bbccfdc3f2c3a523138bfbcd6c0167991eb7b9e6
igorrosenberg <igor.rosenberg@gmail.com>
2013-03-28 17:25:13 +0000

    fine tuning logging

--- examples/gcm/js/log.js
+++ examples/gcm/js/log.js
@@ -12,9 +12,9 @@ function info(string){
 // ----------------------------------
 
 if (!("console" in window)) {
- var names = ["log", "debug", "info", "warn", "error", "assert", "dir", "dirxml", "group", "groupEnd", "time", "timeEnd", "count", "trace", "profile", "profileEnd"];
+ var names = "log,debug,info,warn,error,assert,dir,dirxml,group,groupEnd,time,timeEnd,count,trace,profile,profileEnd".split(",");
  window.console = {};
- for (var i = 0, len = names.length; i < len; ++i) {
- window.console[names[i]] = function(){};
+ for (var i = 0, len = names.length; i < len; i++) {
+   window.console[names[i]] = function(){};
  }
 }		
